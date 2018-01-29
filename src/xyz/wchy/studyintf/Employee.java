a3cc5ecec458d5ca31ed8adc4ef8a86ed59a074e
bincool <chengyiwang9303@foxmail.com>
2018-01-29 15:25:44 +0800

    JavaSE基础-接口和内部类

---
+++ src/xyz/wchy/studyintf/Employee.java
@@ -0,0 +1,34 @@
+/**
+* @FileName: Employee.java
+* @Package: xyz.wchy.studyintf
+* @Copyright: 2018 www.wchy.xyz Inc. All Rights Reserved.
+* @Description: Employee.java: TODO 一句话描述文件的作用.
+* @Author wchy，技术交流(891946049).
+* @Date 2018年1月29日 下午3:23:38.
+* @Content: 新增.
+* @Version: V1.0.
+*/
+package xyz.wchy.studyintf;
+
+/**
+* @ClassName: Employee.java
+* 
+* @Description: 
+* <p>
+* TODO 一句话对这个类进行描述.
+* </p>
+* <p>
+* TODO 详细描述.
+* </p>
+* <p>
+* TODO 示例代码.
+* </p>
+*
+* @Author: wchy，技术交流(891946049).
+* 
+* @Date: 2018年1月29日 下午3:23:38.
+* 
+*/
+public class Employee {
+
+}
