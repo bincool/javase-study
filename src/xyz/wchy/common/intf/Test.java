b3845ccf1297d8dd9b6ba025293cf924e39eef42
bincool <chengyiwang9303@foxmail.com>
2018-06-04 12:40:57 +0800

    测试

---
+++ src/xyz/wchy/common/intf/Test.java
@@ -0,0 +1,42 @@
+/**
+* @FileName: Test.java
+* @Package: xyz.wchy.common.intf
+* @Copyright: 2018 bincool.github.io Inc. All Rights Reserved.
+* @Description: Test.java: TODO 一句话描述文件的作用.
+* @Author wchy，技术交流(891946049).
+* @Date 2018年6月4日 下午12:40:29.
+* @Content: 新增.
+* @Version: V1.0.
+*/
+package xyz.wchy.common.intf;
+
+/**
+* @ClassName: Test.java
+* 
+* @Description: 
+* <p>
+* TODO 一句话对这个类进行描述.
+* </p>
+* <p>
+* TODO 详细描述.
+* </p>
+* <p>
+* TODO 示例代码.
+* </p>
+*
+* @Author: wchy，技术交流(891946049).
+* 
+* @Date: 2018年6月4日 下午12:40:29.
+* 
+*/
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
